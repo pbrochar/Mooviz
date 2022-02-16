eac505e10c657fe19be51535c41a3111cfd5a862
Paul <paul@debian.paul>
2022-02-16 14:36:09 +0100

    change indent to space

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -18,53 +18,53 @@ const Tab = createBottomTabNavigator();
 
 
 const routeIcon = (name, focused) => {
-	const littleSize = 25;
-	const bigSize = 40;
-	const focusedColor = '#e20000';
-	const unfusedColor = 'grey'
+  const littleSize = 25;
+  const bigSize = 40;
+  const focusedColor = '#e20000';
+  const unfusedColor = 'grey'
 
-	switch (name) {
-		case "Playlist":
-			return (
-				<MaterialCommunityIcons 
-					name="filmstrip"
-					color={focused ? focusedColor: unfusedColor}
-					size={focused ? bigSize:littleSize}
-				/>
-			);
-		case "Home" :
-			return (
-				<Icon 
-					name="home"
-					color={focused ? focusedColor: unfusedColor}
-					size={focused ? bigSize:littleSize}
-				/>
-			);
-		case "Friends":
-			return (
-				<Icon 
-					name="users"
-					color={focused ? focusedColor: unfusedColor}
-					size={focused ? bigSize:littleSize} 
-				/>
-			);
-		case "Settings":
-			return (
-				<Icon
-					name="settings"
-					color={focused ? focusedColor: unfusedColor}
-					size={focused ? bigSize:littleSize}  
-				/>
-			);
-		case "Search":
-			return (
-				<Icon 
-					name="search"
-					color={focused ? focusedColor: unfusedColor}
-					size={focused ? bigSize:littleSize} 
-				/>
-			)
-	}
+  switch (name) {
+    case "Playlist":
+      return (
+        <MaterialCommunityIcons 
+          name="filmstrip"
+          color={focused ? focusedColor: unfusedColor}
+          size={focused ? bigSize:littleSize}
+        />
+      );
+    case "Home" :
+      return (
+        <Icon 
+          name="home"
+          color={focused ? focusedColor: unfusedColor}
+          size={focused ? bigSize:littleSize}
+        />
+      );
+    case "Friends":
+      return (
+        <Icon 
+          name="users"
+          color={focused ? focusedColor: unfusedColor}
+          size={focused ? bigSize:littleSize} 
+        />
+      );
+    case "Settings":
+      return (
+        <Icon
+          name="settings"
+          color={focused ? focusedColor: unfusedColor}
+          size={focused ? bigSize:littleSize}  
+        />
+      );
+    case "Search":
+      return (
+        <Icon 
+          name="search"
+          color={focused ? focusedColor: unfusedColor}
+          size={focused ? bigSize:littleSize} 
+        />
+      )
+  }
 }
 
 
@@ -85,32 +85,32 @@ function CustomTabBar({ state, descriptors, navigation }) {
           });
 
           if (!isFocused && !event.defaultPrevented) {
-						//TO DO get the drawer state for close if change tab,
-						// navigation.dispatch(DrawerActions.closeDrawer())
-						// The `merge: true` option makes sure that the params inside the tab screen are preserved
-						navigation.navigate({ name: route.name, merge: true });
-						
+            //TO DO get the drawer state for close if change tab,
+            // navigation.dispatch(DrawerActions.closeDrawer())
+            // The `merge: true` option makes sure that the params inside the tab screen are preserved
+            navigation.navigate({ name: route.name, merge: true });
+            
           }
         };
 
         return (
           <TouchableOpacity
-						accessibilityRole="button"
+            accessibilityRole="button"
             accessibilityState={isFocused ? { selected: true } : {}}
             accessibilityLabel={options.tabBarAccessibilityLabel}
-						onPress={onPress} 
-						style={styles.centeredItems} key={index}>
-						<View
-							style={[styles.centeredItems, styles.itemsNavBar,]}>
-							<View style={{ marginRight: isFocused ? 10: 0,}}>
-								{routeIcon(label, isFocused)}
-							</View>
-							<Text 
-								style={styles.buttonText}>
-								{isFocused ? label: ''}
-							</Text>
-						</View>
-					</TouchableOpacity>
+            onPress={onPress} 
+            style={styles.centeredItems} key={index}>
+            <View
+              style={[styles.centeredItems, styles.itemsNavBar,]}>
+              <View style={{ marginRight: isFocused ? 10: 0,}}>
+                {routeIcon(label, isFocused)}
+              </View>
+              <Text 
+                style={styles.buttonText}>
+                {isFocused ? label: ''}
+              </Text>
+            </View>
+          </TouchableOpacity>
         );
       })}
     </View>
@@ -120,52 +120,52 @@ function CustomTabBar({ state, descriptors, navigation }) {
 
 const BottomTabNavigator = () => {
   return (
-		<SafeAreaProvider>
-			<Tab.Navigator
-				tabBar={(props) => <CustomTabBar {...props} />}
-				initialRouteName="Search" // change to home
-				screenOptions={{
-					headerShown:false,
-					tabBarHideOnKeyboard: true			}}
-			>
-			<Tab.Screen name="Home" component={HomeDrawerNavigator} />
-			<Tab.Screen name="Playlist" component={PlaylistStackNavigator} />
-			<Tab.Screen name="Search" component={SearchStackNavigator} />
-			<Tab.Screen name="Friends" component={FriendsStackNavigator} />
-			<Tab.Screen name="Settings" component={SettingsStackNavigator} />
-		</Tab.Navigator>
-	</SafeAreaProvider>
-	);
+    <SafeAreaProvider>
+      <Tab.Navigator
+        tabBar={(props) => <CustomTabBar {...props} />}
+        initialRouteName="Search" // change to home
+        screenOptions={{
+          headerShown:false,
+          tabBarHideOnKeyboard: true			}}
+      >
+      <Tab.Screen name="Home" component={HomeDrawerNavigator} />
+      <Tab.Screen name="Playlist" component={PlaylistStackNavigator} />
+      <Tab.Screen name="Search" component={SearchStackNavigator} />
+      <Tab.Screen name="Friends" component={FriendsStackNavigator} />
+      <Tab.Screen name="Settings" component={SettingsStackNavigator} />
+    </Tab.Navigator>
+  </SafeAreaProvider>
+  );
 }
 
 const styles = StyleSheet.create({
-	centeredItems: {
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	bottomNavBar : {
-		display: 'flex',
-		flexDirection: 'row', 
-		justifyContent:'space-between',
-		backgroundColor: 'black',
-		height: 70,
-		padding: 10,
-		borderTopWidth: 0.2,
-		borderTopColor: '#525252',
-	},
-	itemsNavBar : {
-		display: 'flex',
-		flexDirection: 'row',
-		borderRadius: 5,
-		paddingRight: 10,
-		paddingLeft: 5,
-		paddingVertical: 5,
-	},
-	buttonText: {
-		color: '#e20000',
-		fontSize: 15,
-		fontWeight: 'bold',
-	}
+  centeredItems: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottomNavBar : {
+    display: 'flex',
+    flexDirection: 'row', 
+    justifyContent:'space-between',
+    backgroundColor: 'black',
+    height: 70,
+    padding: 10,
+    borderTopWidth: 0.2,
+    borderTopColor: '#525252',
+  },
+  itemsNavBar : {
+    display: 'flex',
+    flexDirection: 'row',
+    borderRadius: 5,
+    paddingRight: 10,
+    paddingLeft: 5,
+    paddingVertical: 5,
+  },
+  buttonText: {
+    color: '#e20000',
+    fontSize: 15,
+    fontWeight: 'bold',
+  }
 })
 
 export default BottomTabNavigator;
